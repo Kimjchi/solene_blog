7c7068e0d364f5860bf7a72bb0b9dae71da9238b
Jérémy <kimj77600@gmail.com>
2023-01-04 14:21:50 +0100

    controle line break

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -42,7 +42,7 @@ export default function PostDetail({
           case 'heading-three':
             return <h3 key={index} className={`text-xl font-semibold mb-4 ${className}`}>{modifiedText.map((item:any, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
           case 'paragraph':
-            return <p key={index} className={`mb-8 text-lg ${className}`}>{modifiedText.map((item: any, i:number) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+            return <p key={index} className={`${className.includes("mb-") ? "" : "mb-8"} text-lg ${className}`}>{modifiedText.map((item: any, i:number) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
           case 'heading-four':
             return <h4 key={index} className={`text-md font-semibold mb-4 ${className}`}>{modifiedText.map((item: any, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
           case 'class':
